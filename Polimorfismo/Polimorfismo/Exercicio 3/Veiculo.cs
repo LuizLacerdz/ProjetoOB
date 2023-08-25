5942943af0b7813d8089e9bdd6d9ee5e5ac43920
Gabriel Y. Honji <gabriel.honji78@gmail.com>
2023-08-25 14:56:15 -0300

    Exercicio 3

---
+++ Polimorfismo/Polimorfismo/Exercicio 3/Veiculo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo.Exercicio_3
+{
+    internal class Veiculo
+    {
+        public virtual void CalcGasValue()
+        {
+
+        }
+    }
+}
--- Polimorfismo/Polimorfismo/Program.cs
+++ Polimorfismo/Polimorfismo/Program.cs
@@ -11,29 +11,35 @@ namespace Polimorfismo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Digite o seu salario");
-            Double salario =Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Digite o seu irrf");
-            Double irrf = Convert.ToInt32(Console.ReadLine());
-            Colaborador colaborador = new Colaborador();
-            colaborador.CalcularSalario(salario);
-            colaborador.CalcularIRRF(irrf);
-            Console.WriteLine();
+            // Ex01
+            //Console.WriteLine("Digite o seu salario");
+            //Double salario =Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Digite o seu irrf");
+            //Double irrf = Convert.ToInt32(Console.ReadLine());
+            //Colaborador colaborador = new Colaborador();
+            //colaborador.CalcularSalario(salario);
+            //colaborador.CalcularIRRF(irrf);
+            //Console.WriteLine();
 
-            Professor professor = new Professor();
-            professor.CalcularSalario(salario);
-            professor.CalcularIRRF(irrf);
-            Console.WriteLine();
+            //Professor professor = new Professor();
+            //professor.CalcularSalario(salario);
+            //professor.CalcularIRRF(irrf);
+            //Console.WriteLine();
 
-            Coordenador coordenador = new Coordenador();
-            coordenador.CalcularSalario(salario);
-            coordenador.CalcularIRRF(irrf);
-            Console.WriteLine();
+            //Coordenador coordenador = new Coordenador();
+            //coordenador.CalcularSalario(salario);
+            //coordenador.CalcularIRRF(irrf);
+            //Console.WriteLine();
 
-            Diretor diretor = new Diretor();
-            diretor.CalcularSalario(salario);
-            diretor.CalcularIRRF(irrf);
-            Console.ReadKey();
+            //Diretor diretor = new Diretor();
+            //diretor.CalcularSalario(salario);
+            //diretor.CalcularIRRF(irrf);
+
+            // Ex02
+
+            // Ex03
+            Exercicio_3.Taxi taxi = new Exercicio_3.Taxi();
+            taxi.CalcGasValue();
         }
     }
 }
