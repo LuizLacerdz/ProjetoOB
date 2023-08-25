9b0086e69cfac052c4e7b2af7f6716c54aa59e62
Gustavo <Gubaena01@gmail.com>
2023-08-25 12:11:39 -0300

    Ativiade 1

---
+++ Polimorfismo/Polimorfismo/Program.cs
@@ -0,0 +1,39 @@
+﻿using Polimorfismo.Ativadade_2508;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o seu salario");
+            Double salario =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o seu irrf");
+            Double irrf = Convert.ToInt32(Console.ReadLine());
+            Colaborador colaborador = new Colaborador();
+            colaborador.CalcularSalario(salario);
+            colaborador.CalcularIRRF(irrf);
+            Console.WriteLine();
+
+            Professor professor = new Professor();
+            professor.CalcularSalario(salario);
+            professor.CalcularIRRF(irrf);
+            Console.WriteLine();
+
+            Coordenador coordenador = new Coordenador();
+            coordenador.CalcularSalario(salario);
+            coordenador.CalcularIRRF(irrf);
+            Console.WriteLine();
+
+            Diretor diretor = new Diretor();
+            diretor.CalcularSalario(salario);
+            diretor.CalcularIRRF(irrf);
+            Console.ReadKey();
+        }
+    }
+}
